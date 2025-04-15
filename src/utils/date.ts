e81c5e5f787b0bf8d608b840cf05015ffad6f298
NelsonR-NU <nelson.rondon@nulogic.io>
2025-04-15 08:57:23 -0400

    Storybook fixes and refactor

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,5 @@
-import moment from 'moment'
-import 'moment/locale/es'
+import moment from "moment";
+import "moment/locale/es";
 
 /**
  * Formats the current date in the format "Vier 12 de nov" with proper capitalization
@@ -7,8 +7,54 @@ import 'moment/locale/es'
  * @returns Formatted date string
  */
 export const formatCurrentDate = (locale: string): string => {
-  moment.locale(locale)
+  moment.locale(locale);
   return moment()
-    .format('ddd DD [de] MMM')
-    .replace(/\b[A-zÀ-ú](?=[A-zÀ-ú]{2})/g, (match: string) => match.toUpperCase())
-} 
+    .format("ddd DD [de] MMM")
+    .replace(/\b[A-Za-z\u00C0-\u00FF](?=[A-Za-z\u00C0-\u00FF]{2})/g, (match: string) =>
+      match.toUpperCase()
+    );
+};
+/**
+ * Format the hour from a date string.
+ * @param dateString - Date string in ISO format
+ * @returns Formatted hour string (e.g., "10:30")
+ */
+export const formatHourFromDate = (dateString: string): string => {
+  try {
+    const date = new Date(dateString);
+    if (isNaN(date.getTime())) {
+      return "--:--";
+    }
+
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+
+    return `${hours}:${minutes}`;
+  } catch {
+    // Return default value in case of error
+    return "--:--";
+  }
+};
+
+/**
+ * Format date to display format.
+ * @param dateString - Date string in ISO format
+ * @returns Formatted date string (e.g., "15/10/2023")
+ */
+export const formatDate = (dateString: string): string => {
+  try {
+    const date = new Date(dateString);
+    if (isNaN(date.getTime())) {
+      return "--/--/----";
+    }
+
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  } catch {
+    // Return default value in case of error
+    return "--/--/----";
+  }
+};
