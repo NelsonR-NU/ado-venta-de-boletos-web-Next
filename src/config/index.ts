835dec36687aa558c1dcc9ea95d1bf31190ddeaf
Nagaraj Velusamy <nagaraj.velusamy@nulogic.io>
2025-04-16 17:55:59 +0530

    Removed the TODO comments

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,8 +4,8 @@ export const config = {
     timeout: Number(process.env.NEXT_PUBLIC_API_TIMEOUT) || 30000,
   },
   liferay: {
-    username: process.env.NEXT_PUBLIC_LIFERAY_USERNAME ?? "anjaneya.eswarappa@nulogic.io", // TODO: configure this in the .env file
-    password: process.env.NEXT_PUBLIC_LIFERAY_PASSWORD ?? "Nulogic@123", // TODO: configure this in the .env file
+    username: process.env.NEXT_PUBLIC_LIFERAY_USERNAME ?? "anjaneya.eswarappa@nulogic.io",
+    password: process.env.NEXT_PUBLIC_LIFERAY_PASSWORD ?? "Nulogic@123",
     baseURL:
       process.env.NEXT_PUBLIC_LIFERAY_BASE_URL ??
       "https://front-adoweb-dev.lfr.cloud/o/headless-delivery/v1.0/structured-contents",
